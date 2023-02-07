7feea05dad67508e2a9f178bca90e20075f48514
Simona <simonasablinaite@gmail.com>
2023-02-07 18:33:08 +0200

    Rand function and class

--- react-practice/src/Components/003/Dog.jsx
+++ react-practice/src/Components/003/Dog.jsx
@@ -1,14 +1,22 @@
 // KOMPONENTAS, kurio pavadinimas Dog: 
 // Komponento pavadinimas is didziosios raides. 
 // Komponenta svarbu isexportuoti ir po to suimportuoti reikiamame faile, pvz App.jsx
+// {} skliausteliais pereinama i js aplinka, tai, tai kas yra {} skliausteliuose, yra js kodas
 
-   function Dog() {
+import rand from "../../Functions/rand";
+
+   
+function Dog() {
       return (
          <>
-            <h2>Dog</h2>
-            <h3>Big</h3>
+            <h2>Dog {false ? 'jo' : 'no'} </h2>
+            <h3 className="dog">Big {rand(100, 999)}</h3> 
          </>
       );
    }
 
 export default Dog;
+
+// 13 eil. - random skaiciaus generavimas javascripte
+// {} generuojamas vienos eilutes kodas, todel cia naudinga naudoti ternary operatoriu;
+// 13 eil. - className='dog' - javascriptinis klases uzrasymas. 
