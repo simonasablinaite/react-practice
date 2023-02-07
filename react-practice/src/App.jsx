e34e4a92c8b6a1cfdfa7de254f187c1a3101c3a6
Simona <simonasablinaite@gmail.com>
2023-02-07 16:44:28 +0200

    React app instalated, created komponent

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Dog from './Components/003/Dog';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Dog />
+        <h1>START</h1>
+      <Dog />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-practice/src/Components/003/Dog.jsx
@@ -0,0 +1,14 @@
+// KOMPONENTAS, kurio pavadinimas Dog: 
+// Komponento pavadinimas is didziosios raides. 
+// Komponenta svarbu isexportuoti ir po to suimportuoti reikiamame faile, pvz App.jsx
+
+   function Dog() {
+      return (
+         <>
+            <h2>Dog</h2>
+            <h3>Big</h3>
+         </>
+      );
+   }
+
+export default Dog;
