b9cb76b2aadec5da4de238a5443b480898712b64
Simona <simonasablinaite@gmail.com>
2023-02-08 12:02:31 +0200

    List generation in HTML

---
+++ react-practice/src/003.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Cat from './Components/003/Cat';
+import Dog from './Components/003/Dog';
+import Racoon from './Components/003/Racoon';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Racoon />
+      {/* <Dog /> */}
+        <h1>START</h1>
+      {/* <Cat /> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css';
-import Cat from './Components/003/Cat';
-import Dog from './Components/003/Dog';
-import Racoon from './Components/003/Racoon';
+
+// Masyvas savyje turintis objektus - bene dazniausio duomenu tipo uzduotys, kurias atlikineja reactas
+const cats = [
+   {id: 1, weight: 5, name: 'Pukis'},
+   {id: 2, weight: 3, name: 'Baltis'},
+   {id: 3, weight: 2, name: 'Murka'},
+   {id: 4, weight: 7, name: 'Pilke'},
+   {id: 5, weight: 4, name: 'Juodis'},
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Racoon />
-      {/* <Dog /> */}
-        <h1>START</h1>
-      {/* <Cat /> */}
+            <h1>List</h1>
+            <ul>
+               {
+                  // react'as sumapins cats masyva ir i li elementa sudes katinu vardus. Gausime katinu vardu nerusiuota sarasa
+                  cats.map(cat => <li key={cat.id}>{cat.name}</li>)
+                  // kaip key galima naudoti ir indeksa, jis taip pat yra unikalus. Kodas atrodytu taip:
+                  // cats.map((cat, i) => <li key={i}>{cat.name}</li>)
+               }
+            </ul>
       </header>
     </div>
   );
