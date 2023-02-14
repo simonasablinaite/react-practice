48bbb3e04074d7a3551b826a0e798c513c522547
Simona <simonasablinaite@gmail.com>
2023-02-14 11:20:55 +0200

    Created button style

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <div className="App">
          <header className="App-header">
             <h1>State</h1>
+            <button className='glow-btn' type='button'>Go</button>
          </header>
       </div>
    );
