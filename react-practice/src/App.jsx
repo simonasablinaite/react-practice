c706bc7675d231fc47defa605fda6622a7fb9320
Simona <simonasablinaite@gmail.com>
2023-02-14 19:43:59 +0200

    State

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,20 +1,33 @@
+import { useState } from 'react';
 import './App.scss';
 
 
 function App() {
 
-   const doJob = () => {
-      console.log('Eik dirbti!');
-   }
+   // state sukurimas
+   // Sukuria masyva su 2 reiksmem
+   const [color, setColor] = useState('yellow');
+
+   /*Paaiskinimas: useState('yellowGreen') f-ja grazina masyva su dviem reiksmem:
+   const color; //state
+   const setColor; //f-ja state keitimui
+   'yellowgreen' - pradine state reikme pries pakeiciant ja
+   */
+
+
+   // Sios funkcijos bendriniu atveju vadinamos hook'ais
+   // const doJob = () => {
+   //    console.log('Eik dirbti!');
+   // }
 
    const clicked = () => {
-      doJob();
+      setColor('lime'); //state keitimas
    }
 
    return (
       <div className="App">
          <header className="App-header">
-            <h1>State</h1>
+            <h1 style={{ color: color }}>State</h1>
             <button onClick={clicked} className='glow-btn' type='button'>Go</button>
          </header>
       </div>
@@ -31,4 +44,11 @@ Tai reiskia, kad paspaudus mygtuka, konsole turi isspausdinti zodi 'Labas';
 Galima funkcijai "clicked" nurodyti vykdyti kita f-ja, pvz, "doJob":
 Sukuriamas naujas componentas f-jai doJob -> ja paduodame f-jai clicked -> paspaudus mygtuka, bus iskviesta doJob f-ja
 
+State kurimo principas:
+const M = ['A', 'B']; // konstanta M lygi masyvui, kuriame reiksmes A ir B;
+const [x, y] = M // destruktorius, kuris sifruojasi taip:
+const x = 'A' // x reiksme lygi M masyvo A reiksmei
+const y = 'B' // y reiksme lygi M masyvo B reiksmei
+
+<hi style{{color: color}} - state panaudojimas
 */
