f2fcc23d3678ecbb07951a804128eeff1bdd681b
Simona <simonasablinaite@gmail.com>
2023-02-14 12:18:24 +0200

    Start function when clicked the button

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -2,14 +2,35 @@ import './App.scss';
 
 
 function App() {
+
+   const doJob = () => {
+      console.log('Eik dirbti!');
+   }
+
+   const clicked = () => {
+      doJob();
+   }
+
    return (
       <div className="App">
          <header className="App-header">
             <h1>State</h1>
-            <button className='glow-btn' type='button'>Go</button>
+            <button onClick={clicked} className='glow-btn' type='button'>Go</button>
          </header>
       </div>
    );
 }
 
 export default App;
+
+/* Mygtuko veikimo principas:
+Mygtukui nurodomas eventas "onClick" ir i jo vidu paduodama funkcija, ka ,ygtukas turi daryti, ji paspaudus.
+Reikia buti susikurus paduotaja f-ja, kuri siuo atveju yra "clicked". 
+Tai rei6kia, kad paspaudus mygtuka, konsole turi isspausdinti zodi 'Labas'
+
+Galima funkcijai "clicked" nurodyti vykdyti kita f-ja, pvz, "doJob":
+Sukuriamas naujas componentas f-jai doJob -> ja paduodame f-jai clicked -> paspaudus mygtuka, bus iskviesta doJob f-ja
+
+
+
+*/
